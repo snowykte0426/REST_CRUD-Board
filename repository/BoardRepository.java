72ff4d4bfd02ebfcc7d64208c8543063c6e7324b
KIM TAEEUN <snowykte0426@naver.com>
2024-07-10 18:30:34 +0900

    Delete repository directory

--- repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.riotdo.REST_Board.repository;
-
-import com.example.riotdo.REST_Board.Entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-}
